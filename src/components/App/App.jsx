7dc647804ce8271d0f89ebc531ac8dfd046f890e
aasishadhikari <gemini.aasis@gmail.com>
2020-05-09 15:45:57 +0930

    Installed aframe-react package

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,40 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {Route, Switch} from 'react-router-dom';
-import Header from '../Header';
+import { Route, Switch } from 'react-router-dom';
 import styles from './App.scss';
 import Index from '../Index';
-import About from '../About';
-import NotFound from '../NotFound';
 
 class App extends Component {
+  constructor(props) {
+    super();
 
-	constructor(props) {
-		super();
+    this.props = props;
+  }
 
-		this.props = props;
-	}
+  static propTypes = {};
 
-	static propTypes = {
-	};
+  render() {
+    const basePath = this.props.basePath || '/';
 
-	render() {
-	const basePath = this.props.basePath || '/';
-
-	return (
-		<div>
-			<Header />
-			<div className={styles.appBody}>
-				<Switch>
-					<Route exact path={basePath} component={Index} />
-					<Route path={`${basePath}about`} component={About} />
-					<Route component={NotFound} />
-				</Switch>
-			</div>
-		</div>
-	);
-	}
-
+    return (
+      // <div className={styles.appBody}>
+      <Switch>
+        <Route exact path={basePath} component={Index} />
+      </Switch>
+    );
+  }
 }
 
 export default App;
